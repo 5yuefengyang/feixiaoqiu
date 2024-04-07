bc68191b0e363c9d1d5d8acbb0741c34a249c401
4o3F <4o3f@proton.me>
2024-04-07 21:06:57 +0800

    feat: Init configuration encryption and database connection

--- backend/src/main/java/cafe/hoyoverse/hsr/HonkaiStarRailApplication.java
+++ backend/src/main/java/cafe/hoyoverse/hsr/HonkaiStarRailApplication.java
@@ -1,14 +1,23 @@
 package cafe.hoyoverse.hsr;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
-@ConfigurationPropertiesScan({"cafe.hoyoverse.hsr.config"})
+@EnableEncryptableProperties
 public class HonkaiStarRailApplication {
 
     public static void main(String[] args) {
+//        BasicTextEncryptor textEncryptor = new BasicTextEncryptor();
+//        //加密所需的salt(盐)
+//        textEncryptor.setPassword("");
+//        //要加密的数据（数据库的用户名或密码）
+//        String mysql_username = textEncryptor.encrypt("xxxxxx");
+//        String mysql_password = textEncryptor.encrypt("xxxxxx");
+//        System.out.println("username:"+mysql_username);
+//        System.out.println("password:"+mysql_password);
+
         SpringApplication.run(HonkaiStarRailApplication.class, args);
     }
 }
--- backend/src/main/java/cafe/hoyoverse/hsr/config/ServerConfig.java
+++
@@ -1,8 +0,0 @@
-package cafe.hoyoverse.hsr.config;
-
-import org.springframework.boot.context.properties.ConfigurationProperties;
-
-@ConfigurationProperties(prefix = "server")
-public class ServerConfig {
-    private int port = 2333;
-}
